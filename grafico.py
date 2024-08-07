710726ce6fd331ec19050e1b258e880ee0b20dce
DanielHRamos <daniel.huamani@correo.unimet.edu.ve>
2024-08-07 16:50:29 -0400

    Colocado comentario acerca del grafico en grafico.py

--- grafico.py
+++ grafico.py
@@ -5,7 +5,8 @@ df = pan.read_csv(r"C:\Users\Daniel\Desktop\starwars\csv\characters.csv")
 
 
 pjs_por_planeta = df["homeworld"].value_counts()
-
+# son 96 en total, pero el nro 19: Dron BB-8 tiene valor "None" en su carateristica
+# "homeworld" asi que el value_counts() no lo toma en cuenta.
 
 plot.figure(figsize=(12,8))
 plot.bar(pjs_por_planeta.index, pjs_por_planeta.values)
