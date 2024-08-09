658e639d5fcf09aa8dd7ac49e48246649b69fea8
DanielHRamos <daniel.huamani@correo.unimet.edu.ve>
2024-08-09 15:20:55 -0400

    -Agregado a prueba.py un menu para probar de forma mas comoda la funcionalidad de cada modulo
    -Agregado a Grafico.py una clase llamada Grafico2
    <nombre provisional>, que cumple la funcion de hacer
    graficos de varias caracteristicas de las naves

--- grafico.py
+++ grafico.py
@@ -5,13 +5,13 @@ class Grafico:
     def __init__(self, data):
         self.df = pan.read_csv(data)
         
-    def contar_pjs_por_planeta(self):
+    def pjs_por_planeta(self):
         return self.df["homeworld"].value_counts()
     
     def mostrar_grafico(self,figsize=(12,8)):
         # son 96 en total, pero el nro 19: Dron BB-8 tiene valor "None" en su carateristica
         # "homeworld" asi que el value_counts() no lo toma en cuenta.
-        pjs = self.contar_pjs_por_planeta()
+        pjs = self.pjs_por_planeta()
         plot.figure(figsize=figsize)
         plot.bar(pjs.index, pjs.values)
         plot.xticks(rotation = 90)
@@ -19,3 +19,66 @@ class Grafico:
         plot.xlabel("Planeta")
         plot.ylabel("Numero de personajes")
         plot.show()
+
+class Grafico2:
+    def __init__(self, data):
+        self.df =  pan.read_csv(data)
+    def nombre_nave(self):
+        return self.df["name"]
+    
+    def longitud_naves(self):
+        return self.df["length"]
+    
+    def capacidad_carga(self):
+        return self.df["cargo_capacity"]
+    
+    def clasif_hiper(self):
+        return self.df["hyperdrive_rating"]
+    
+    def mglt(self):
+        return self.df["MGLT"]
+    
+    def mostrar_grafico(self,figsize=(12,8)):
+        # en este grafico hay 8 naves que tienen exactamente los mismos valores en cada atributo, por lo tanto
+        # se omiten asi que de 60 naves que aparecen en el archivo cvs solo aparecen 52 en el grafico
+        name = self.nombre_nave()
+        
+        long = self.longitud_naves()
+        plot.figure(figsize=figsize)
+        plot.title("Longitud de las naves")
+        plot.bar(name.values, long.values)
+        plot.xlabel("Naves")
+        plot.ylabel("Longitud")
+        plot.xticks(rotation=90)
+        plot.show()
+        
+        
+        cap = self.capacidad_carga()
+        plot.figure(figsize=figsize)
+        plot.title("Capacidad de carga de las naves")
+        plot.bar(name.values,cap.values)
+        plot.xlabel("Naves")
+        plot.ylabel("Capacidad de carga (x10^8)")
+        plot.xticks(rotation=90)
+        plot.show()
+        
+        
+        clas = self.clasif_hiper()
+        plot.figure(figsize=figsize)
+        plot.title("Clasificacion de hiperimpulsor")
+        plot.bar(name.values, clas.values)
+        plot.xlabel("Naves")
+        plot.ylabel("Clasificacion")
+        plot.xticks(rotation=90)
+        plot.show()
+        
+        
+        mglt = self.mglt()
+        plot.figure(figsize=figsize)
+        plot.title("Modern Galactic Light Time (MGLT)")
+        plot.bar(name.values, mglt.values)
+        plot.xlabel("Naves")
+        plot.ylabel("MGLT")
+        plot.xticks(rotation=90)
+        plot.show()
+        
--- prueba.py
+++ prueba.py
@@ -1,10 +1,11 @@
 from db import load_films
 from Film import Film
-from Grafico import Grafico
+from Grafico import Grafico, Grafico2
 
 
 def main():
     peliculas = []
+    
     for film in load_films()["result"]:
         peliculas.append(Film(film["properties"]["title"],
                          film["properties"]["episode_id"],
@@ -12,9 +13,28 @@ def main():
                          film["properties"]["opening_crawl"],
                          film["properties"]["director"]))
         
-    for pelicula in peliculas:
-        pelicula.show_attr()
+
     
-    graphic = Grafico(r"C:\Users\Daniel\Desktop\starwars\csv\characters.csv")
-    graphic.mostrar_grafico()
+    while True:
+        opcion = input(f"1. Mostrar peliculas.\n2. Mostrar grafico de personajes nacidos en cada planeta. \n3. Mostrar graficos de caracteristicas de naves.\n0. Salir.\n\n\n>>> ")
+        
+        if opcion == "1":
+            for pelicula in peliculas:
+                pelicula.show_attr()
+        
+        elif opcion == "2":
+            graphic = Grafico(r"C:\Users\Daniel\Desktop\starwars\csv\characters.csv")
+            graphic.mostrar_grafico()
+            
+        elif opcion == "3":
+            graphic1 = Grafico2(r"C:\Users\Daniel\Desktop\starwars\csv\starships.csv")
+            graphic1.mostrar_grafico()
+            
+        elif opcion == "0":
+            print("Gracias por usar el programa, que la fuerza te acompañe.")
+            break
+        
+        else:
+            print("Error, ingrese una opcion valida.")
+             
 main()
